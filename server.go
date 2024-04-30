74c948a249cdc076aebe3a68a1bd5721aba789d8
LeAnne B <branchleannelmb@gmail.com>
2024-04-30 16:48:28 -0400

    swaps out echo for Gin

--- server.go
+++ server.go
@@ -1,66 +1,20 @@
 package main
 
 import (
-	"html/template"
-	"io"
 	"net/http"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/gin-gonic/gin"
 )
 
-type Templates struct {
-	templates *template.Template
-}
-
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func NewTemplates() *Templates {
-	return &Templates{
-		templates: template.Must(template.ParseGlob("*.html")),
-	}
-}
 
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
+	router := gin.Default()
 
-	e.Renderer = NewTemplates()
+	router.LoadHTMLGlob("pages/*.html")
 
-	e.GET("/", func(c echo.Context) error {
-		
-		return c.Render(http.StatusOK, "index", "data")
+	router.GET("/", func(c *gin.Context){
+		c.HTML(http.StatusOK, "index.html", "")
 	})
 
-	e.GET("/sorcerer", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "sorcerer", "buildinfo")
-	})
-
-	e.GET("/necromancer", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "necro", "buildinfo")
-	})
-
-	e.GET("/arcanist", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "arcanist", "buildinfo")
-	})
-
-	e.GET("/warden", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "warden", "buildinfo")
-	})
-
-	e.GET("/dragonknight", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "dk", "buildinfo")
-	})
-
-	e.GET("/nightblade", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "nb", "buildinfo")
-	})
-
-	e.GET("/templar", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "templar", "buildinfo")
-	})
-
-	e.Logger.Fatal(e.Start(":1323"))
+	router.Run();
 }
